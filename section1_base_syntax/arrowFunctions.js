1a21e2a23ebb6d59cca54e2439e0194c9cf22dbc
Fubuki Miyashita <miyasita.fubuki.jk@mynavi.jp>
2021-12-27 01:40:57 +0000

    first section files

---
+++ section1_base_syntax/arrowFunctions.js
@@ -0,0 +1,9 @@
+const pen = "I'm Pen";
+const friend = "No, you are Tom";
+
+const print = (tom, friend) => {
+  console.log(tom)
+  console.log(friend)
+}
+
+print(pen, friend)
---
+++ section1_base_syntax/functions.js
@@ -0,0 +1,9 @@
+const pen = "I'm Pen";
+const friend = "No, you are Tom";
+
+function print (tom, friend) {
+  console.log(tom)
+  console.log(friend)
+}
+
+print(pen, friend)
